88b7318b947bdc3cc39f1d66036006312c20fa96
Brayan Bernal <brayanbernal0710@gmail.com>
2020-11-10 23:41:38 -0400

    Adding minor changes into views and index

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
+<?php namespace App;
+use \Views\Home;
+use \Views\RegisterHouse;
+use \Views\Login;
+use \Views\RegisterUser;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title>
 </head>
 <body>
-    Esto es index
+    <?php ?>
 </body>
 </html>
